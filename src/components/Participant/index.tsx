30e87dace7e5e45e3595498c6fae61c5b6d2d43f
faelcintra <rafaacintramartins@gmail.com>
2023-08-22 19:00:49 -0300

    feat: :sparkles: Use ScrollView for displaying participants

--- src/components/Participant/index.tsx
+++ src/components/Participant/index.tsx
@@ -1,14 +1,18 @@
-import { Text, TouchableOpacity, View } from "react-native";
-import { styles } from './styles'
+import { Text, TouchableOpacity, View } from 'react-native';
+import { styles } from './styles';
 
-export function Participant () {
+type Props = {
+  name: string;
+  onRemove: () => void;
+};
+export function Participant({ name, onRemove }: Props) {
   return (
     <View style={styles.container}>
-        <Text style={styles.name}>Fael Cintra</Text>
+      <Text style={styles.name}>{name}</Text>
 
-        <TouchableOpacity style={styles.buttonContainer} >
+      <TouchableOpacity style={styles.buttonContainer} onPress={onRemove}>
         <Text style={styles.buttonText}>+</Text>
       </TouchableOpacity>
     </View>
-  )
+  );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,33 +1,58 @@
-import { Text,TextInput ,TouchableOpacity,View } from "react-native";
-import {styles} from './styles'
-import { Participant } from "../../components/Participant";
+import {
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { styles } from './styles';
+import { Participant } from '../../components/Participant';
 
-export function Home () {
-
-  function handleAdd () {
+export function Home() {
+  const participants = [
+    'Rafael',
+    'Samuel',
+    'Caio',
+    'João',
+    'Matheus',
+    'Charles',
+    'Eduardo',
+  ];
+  function handleParticipantAdd() {
     console.log('clicou aqui mesmo');
-    
   }
-  return ( 
+
+  function handleParticipantRemove(name: string) {
+    console.log('voce deseja remover', name);
+  }
+
+  return (
     <View style={styles.container}>
       <Text style={styles.eventName}>teste</Text>
       <Text style={styles.eventDate}>Terça, 22 de agosto de 2023</Text>
-
       <View style={styles.form}>
-      <TextInput 
-        style={styles.input}
-        placeholder="Nome do participante"
-        placeholderTextColor="#6B6B6B" 
-        keyboardType="twitter"
-      />
-      <TouchableOpacity  style={styles.buttonContainer} onPress={handleAdd}>
-        <Text style={styles.buttonText}>+</Text>
-      </TouchableOpacity>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+          keyboardType="twitter"
+        />
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={handleParticipantAdd}
+        >
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
       </View>
-
-      <Participant/>
-      <Participant/>
-
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {participants.map((participant) => (
+          <Participant
+            key={participant}
+            name={participant}
+            onRemove={() => handleParticipantRemove('teste')}
+          />
+        ))}
+      </ScrollView>
     </View>
-  )
+  );
 }
